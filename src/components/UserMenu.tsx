1865db079247e4e0c51f0c59b65bd3c89fc20989
volochayev <stingray8888831@gmail.com>
2025-07-21 21:14:49 +0300

    add some

---
+++ src/components/UserMenu.tsx
@@ -0,0 +1,8 @@
+export default function UserMenu() {
+    return (
+        <div>
+            <p>Welcome, MAngo</p>
+            <button>Log Out</button>
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import "modern-normalize";
+import UserMenu from './components/UserMenu';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+// const jsxMarkup = "Jacob";
+
+// const imgUrl = "https://images.app.goo.gl/d7oNR2ifYfpVRPrd7";
+//   <>
+//     <img src={imgUrl} alt="Jackie" width="320" />
+//     <p>HTML in JS?</p>
+//   </>
+
+const jsxMarkup = (
+  <>
+    <UserMenu/>
+    {[1, 2, 3, 4, 5, 6, 7].map((value, index) => {
+      return <li key={index}>{value}</li>
+    })}
+  </>
 )
+createRoot(document.getElementById('root')! as HTMLDivElement).render(jsxMarkup);
