4ae2d2c31055a278d2bff39cd5efd82ba0c768c7
volochayev <stingray8888831@gmail.com>
2025-07-29 15:09:23 +0300

    add so

--- src/components/ClickCounter.tsx
+++ src/components/ClickCounter.tsx
@@ -1,15 +1,1 @@
 import { useState } from "react";
-
-export default function ClickCounter() {
-    const [clicks, setClicks] = useState(0);
-    const handleClick = () => {
-        setClicks(clicks + 1);
-                
-    };
-
-    return (
-        <>
-            <button onClick={handleClick}>Click {clicks}</button>
-        </>
-    );
-}
