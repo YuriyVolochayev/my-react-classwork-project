fe9a6116e5ddf036ec2a7ef7483954d6fdfe39ae
volochayev <stingray8888831@gmail.com>
2025-08-11 22:00:25 +0300

    add some

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,54 +1,88 @@
-import { useState } from "react";
-import { Toaster } from "sonner";
-import { useQuery, keepPreviousData } from "@tanstack/react-query";
-import ReactPaginate from "react-paginate";
-import SearchForm from "./SearchForm";
-import ArticleList from "./ArticleList";
-import { fetchArticles } from "../services/articleService";
-import css from "./App.module.css"
+// import { useState } from "react";
+// import { Toaster } from "sonner";
+// import { useQuery, keepPreviousData } from "@tanstack/react-query";
+// import ReactPaginate from "react-paginate";
+// import SearchForm from "./SearchForm";
+// import ArticleList from "./ArticleList";
+// import { fetchArticles } from "../services/articleService";
+// import css from "./App.module.css"
 
+
+// export default function App() {
+//   const [searchTopic, setSearchTopic] = useState("");
+//   const [currentPage, setCurrentPage] = useState(1);
+
+//   const { data, isLoading, isError, isSuccess } = useQuery({
+//     queryKey: ["articles", searchTopic, currentPage],
+//     queryFn: () => fetchArticles(searchTopic, currentPage),
+//     enabled: searchTopic !== "",
+//     placeholderData: keepPreviousData,
+//   });
+
+//   const totalPages = data?.nbPages || 0;
+
+//   const handleSearch = (newTopic: string) => {
+//     setSearchTopic(newTopic);
+//   };
+
+//   return (
+//     <>
+//       <SearchForm onSubmit={handleSearch} />
+
+//       {isSuccess && totalPages > 1 && (
+//         <ReactPaginate
+//           breakLabel="..."
+//           nextLabel=">"
+//           previousLabel="<"
+//           pageCount={totalPages}
+//           pageRangeDisplayed={5}
+//           marginPagesDisplayed={3}
+//           onPageChange={({ selected }) => setCurrentPage(selected + 1)}
+//           forcePage={currentPage - 1}
+//           containerClassName={css.pagination}
+//           activeClassName={css.active}
+//         />
+//       )}
+
+//       {isLoading && <strong>Loading data...</strong>}
+//       {isError && <strong>Error...</strong>}
+//       {data?.hits && <ArticleList items={data.hits} />}
+
+//       <Toaster position="top-right" duration={2000} />
+//     </>
+//   );
+// }
+
+// 1. Імпортуємо хук useMutation
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 
 export default function App() {
-  const [searchTopic, setSearchTopic] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const { data, isLoading, isError, isSuccess } = useQuery({
-    queryKey: ["articles", searchTopic, currentPage],
-    queryFn: () => fetchArticles(searchTopic, currentPage),
-    enabled: searchTopic !== "",
-    placeholderData: keepPreviousData,
+	// 2. Використовуємо хук
+	const mutation = useMutation({
+    mutationFn: async (newTodo) => {
+      const res = await axios.post('https://jsonplaceholder.typicode.com/todos', newTodo);
+      return res.data;
+    },
+    onSuccess: () => {
+			console.log("Todo added successfully");
+    }
   });
-
-  const totalPages = data?.nbPages || 0;
-
-  const handleSearch = (newTopic: string) => {
-    setSearchTopic(newTopic);
+  
+  const handleCreateTodo = () => {
+	  // 3. Викликаємо mutate для того щоб виконати HTTP-запит
+	  mutation.mutate({
+		  title: "My new todo",
+		  completed: false
+	  })
   };
 
   return (
     <>
-      <SearchForm onSubmit={handleSearch} />
-
-      {isSuccess && totalPages > 1 && (
-        <ReactPaginate
-          breakLabel="..."
-          nextLabel=">"
-          previousLabel="<"
-          pageCount={totalPages}
-          pageRangeDisplayed={5}
-          marginPagesDisplayed={3}
-          onPageChange={({ selected }) => setCurrentPage(selected + 1)}
-          forcePage={currentPage - 1}
-          containerClassName={css.pagination}
-          activeClassName={css.active}
-        />
-      )}
-
-      {isLoading && <strong>Loading data...</strong>}
-      {isError && <strong>Error...</strong>}
-      {data?.hits && <ArticleList items={data.hits} />}
-
-      <Toaster position="top-right" duration={2000} />
+	    <button onClick={handleCreateTodo}>Create Todo</button>
+	    {mutation.isPending && <div>Adding todo...</div>}
+	    {mutation.isError && <div>An error occurred</div>}
+	    {mutation.isSuccess && <div>Todo added!</div>}
     </>
   );
 }
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,23 +1,21 @@
-// src/Timer.tsx
+import { useEffect, useState } from 'react';
 
-// import { useEffect, useState } from 'react';
+export default function Timer() {
+  const [time, setTime] = useState(new Date());
 
-// export default function Timer() {
-//   const [time, setTime] = useState(new Date());
+  useEffect(() => {
+	  // 1. Зберігаєм ідентифікатор інтервалу в змінну
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+      console.log(`Interval - ${Date.now()}`);
+    }, 1000);
 
-//   useEffect(() => {
-// 	  // 1. Зберігаєм ідентифікатор інтервалу в змінну
-//     const intervalId = setInterval(() => {
-//       setTime(new Date());
-//       console.log(`Interval - ${Date.now()}`);
-//     }, 1000);
+    return () => {
+	    // 2. Видаляємо інтервал за його id
+      clearInterval(intervalId);
+    };
+  }, []);
 
-//     return () => {
-// 	    // 2. Видаляємо інтервал за його id
-//       clearInterval(intervalId);
-//     };
-//   }, []);
-
-//   return <p>TimerBox - {time.toLocaleTimeString()}</p>;
-// }
+  return <p>TimerBox - {time.toLocaleTimeString()}</p>;
+}
 
